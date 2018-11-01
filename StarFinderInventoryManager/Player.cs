3e71aa80dab9cdedc9e23e8c710e908dd61a6ae6
Tessa Franklin <tematejc@microsoft.com>
2018-10-31 17:38:08 -0700

    Add project files.

---
+++ StarFinderInventoryManager/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarFinderInventoryManager
+{
+ /// <summary>
+ /// Player Class that will contain player data.
+ /// </summary>
+    public class Player
+    {
+        /// <summary>
+        /// Player Specific information
+        /// </summary>
+        public class Profile
+        {
+
+        }
+
+        /// <summary>
+        /// Player ability information
+        /// </summary>
+        public class Abilities
+        {
+
+        }
+
+        /// <summary>
+        /// Player Skill information
+        /// </summary>
+        public class Skills
+        {
+
+        }
+
+        /// <summary>
+        /// Player health, status, and throws
+        /// </summary>
+        public class Status
+        {
+
+        }
+
+        /// <summary>
+        /// Player's equipment, weapons and armor
+        /// </summary>
+        public class Equipment
+        {
+
+        }
+
+        
+        public class Inventory
+        {
+
+        }
+    }
+}
